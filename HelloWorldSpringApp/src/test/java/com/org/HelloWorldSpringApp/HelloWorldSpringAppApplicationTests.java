96bde62977f399c1115274a7604649025f1422a1
Pooja14206 <pooja.14206@gmail.com>
2021-08-08 12:39:34 +0530

    Create a file for HelloWorldApp

---
+++ HelloWorldSpringApp/src/test/java/com/org/HelloWorldSpringApp/HelloWorldSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.HelloWorldSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
